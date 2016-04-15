2688d59f69abeaf4eab97c1ff74e7584b5e296bc
Niklas Casaril <niklas@nipe.se>
2016-04-15 18:40:02 +1000

    Added test marker

--- ros/cpp/locationtracker.cpp
+++ ros/cpp/locationtracker.cpp
@@ -15,6 +15,7 @@
 
 // ROS
 #include "ros/ros.h"
+#include <visualization_msgs/Marker.h>
 #include <lps/LPSRange.h>
 
 // System V IPC
@@ -430,6 +431,9 @@ int main(int argc, char *argv[])
     ros::NodeHandle n;
     
     ros::Subscriber sub = n.subscribe("lpsranges", 20, lpsrangeCallback);
+    ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+
+    uint32_t shape = visualization_msgs::Marker::CUBE;
 
     ROS_INFO("MysqlThread: Connecting to database");
     _mw = new MysqlWrap("localhost", "md_usr", "peoplesleepingbetweensheets", "mddb");
@@ -448,9 +452,48 @@ int main(int argc, char *argv[])
     }
 
     double updated_map_and_tags = 0;
+    double updated_markers = 0;
     ros::Rate loop_rate(15);
     while (ros::ok())
     {
+        if (ros::Time::now().toSec() - updated_markers > 1.0)
+        {
+            visualization_msgs::Marker marker;
+            marker.header.frame_id = "/my_frame";
+            marker.header.stamp = ros::Time::now();
+            // Set the namespace and id for this marker.  This serves to create a unique ID
+            // Any marker sent with the same namespace and id will overwrite the old one
+            marker.ns = "basic_shapes";
+            marker.id = 0;
+            marker.type = shape;
+            
+            // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+            marker.action = visualization_msgs::Marker::ADD;
+
+            marker.pose.position.x = 0;
+            marker.pose.position.y = 0;
+            marker.pose.position.z = 0;
+            marker.pose.orientation.x = 0.0;
+            marker.pose.orientation.y = 0.0;
+            marker.pose.orientation.z = 0.0;
+            marker.pose.orientation.w = 1.0;
+            
+            // Set the scale of the marker -- 1x1x1 here means 1m on a side
+            marker.scale.x = 1.0;
+            marker.scale.y = 1.0;
+            marker.scale.z = 1.0;
+            
+            // Set the color -- be sure to set alpha to something non-zero!
+            marker.color.r = 0.0f;
+            marker.color.g = 1.0f;
+            marker.color.b = 0.0f;
+            marker.color.a = 1.0;
+            
+            marker.lifetime = ros::Duration();
+            marker_pub.publish(marker);
+            ROS_INFO("Published marker");
+        }
+
         if (ros::Time::now().toSec() - updated_map_and_tags > 30.0)
         {
             updated_map_and_tags = ros::Time::now().toSec();
